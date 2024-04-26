d236feae2f35341f270c95e0d743728b00af64c1
Jackson Nazário Salvador Guimarães <jacksonnsalvador@gmail.com>
2024-04-26 11:37:49 -0300

    Entity user and REST funcionando

---
+++ src/main/java/com/jacknsalv/workshopmongodb/domain/Comment.java
@@ -0,0 +1,12 @@
+package com.jacknsalv.workshopmongodb.domain;
+
+import java.util.Date;
+
+public class Comment {
+	
+	private String id;
+	private String text;
+	private Date date;
+		
+
+}
---
+++ src/main/java/com/jacknsalv/workshopmongodb/domain/Post.java
@@ -0,0 +1,11 @@
+package com.jacknsalv.workshopmongodb.domain;
+
+import java.util.Date;
+
+public class Post {
+
+	private String id;
+	private Date date;
+	private String title;
+	private String body;
+}
