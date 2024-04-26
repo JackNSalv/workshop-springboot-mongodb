fc59eb536cdbe276bcffaf9a40c0bfa241491092
Jackson Nazário Salvador Guimarães <jacksonnsalvador@gmail.com>
2024-04-26 16:03:44 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/jacknsalv/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/jacknsalv/workshopmongodb/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.jacknsalv.workshopmongodb.domain.Post;
 import com.jacknsalv.workshopmongodb.domain.User;
 import com.jacknsalv.workshopmongodb.dto.UserDTO;
 import com.jacknsalv.workshopmongodb.services.UserService;
@@ -42,6 +43,12 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);		
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 	@PostMapping
