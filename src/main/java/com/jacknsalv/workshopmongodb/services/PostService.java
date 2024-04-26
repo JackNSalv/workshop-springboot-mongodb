81ef842eb14b13507d4f5ffa4191916d4403b20e
Jackson Nazário Salvador Guimarães <jacksonnsalvador@gmail.com>
2024-04-26 16:07:05 -0300

    Obtendo post por id

---
+++ src/main/java/com/jacknsalv/workshopmongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.jacknsalv.workshopmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jacknsalv.workshopmongodb.domain.Post;
+import com.jacknsalv.workshopmongodb.repositories.PostRepository;
+import com.jacknsalv.workshopmongodb.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
